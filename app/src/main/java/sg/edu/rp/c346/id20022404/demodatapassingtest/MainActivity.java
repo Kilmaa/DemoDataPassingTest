0a6a4a2c228701bb9c39b21eb6100e288b43a6bb
Kilmaa <20022404@myrp.edu.sg>
2021-05-19 12:14:27 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id20022404/demodatapassingtest/MainActivity.java
@@ -0,0 +1,31 @@
+package sg.edu.rp.c346.id20022404.demodatapassingtest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnPassInteger;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        btnPassInteger = findViewById(R.id.buttonPassInteger);
+
+        btnPassInteger.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                intent.putExtra("value", 170);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
